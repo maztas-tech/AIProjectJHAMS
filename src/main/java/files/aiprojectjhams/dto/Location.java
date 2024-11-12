c784f6969194eccb8dde4ad1d6e1a993f0196d73
maztas-tech <mata0001@stud.kea.dk>
2024-11-12 19:01:54 +0100

    added dto and rest controller for weather app Not finished yet. I added so the other members of the team can work on it, or need to have a extra look

---
+++ src/main/java/files/aiprojectjhams/dto/Location.java
@@ -0,0 +1,170 @@
+
+package files.aiprojectjhams.dto;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.annotation.processing.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "name",
+    "country",
+    "region",
+    "lat",
+    "lon",
+    "timezone_id",
+    "localtime",
+    "localtime_epoch",
+    "utc_offset"
+})
+@Generated("jsonschema2pojo")
+public class Location {
+
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("country")
+    private String country;
+    @JsonProperty("region")
+    private String region;
+    @JsonProperty("lat")
+    private String lat;
+    @JsonProperty("lon")
+    private String lon;
+    @JsonProperty("timezone_id")
+    private String timezoneId;
+    @JsonProperty("localtime")
+    private String localtime;
+    @JsonProperty("localtime_epoch")
+    private Integer localtimeEpoch;
+    @JsonProperty("utc_offset")
+    private String utcOffset;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public Location() {
+    }
+
+    public Location(String name, String country, String region, String lat, String lon, String timezoneId, String localtime, Integer localtimeEpoch, String utcOffset) {
+        super();
+        this.name = name;
+        this.country = country;
+        this.region = region;
+        this.lat = lat;
+        this.lon = lon;
+        this.timezoneId = timezoneId;
+        this.localtime = localtime;
+        this.localtimeEpoch = localtimeEpoch;
+        this.utcOffset = utcOffset;
+    }
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("country")
+    public String getCountry() {
+        return country;
+    }
+
+    @JsonProperty("country")
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @JsonProperty("region")
+    public String getRegion() {
+        return region;
+    }
+
+    @JsonProperty("region")
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    @JsonProperty("lat")
+    public String getLat() {
+        return lat;
+    }
+
+    @JsonProperty("lat")
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    @JsonProperty("lon")
+    public String getLon() {
+        return lon;
+    }
+
+    @JsonProperty("lon")
+    public void setLon(String lon) {
+        this.lon = lon;
+    }
+
+    @JsonProperty("timezone_id")
+    public String getTimezoneId() {
+        return timezoneId;
+    }
+
+    @JsonProperty("timezone_id")
+    public void setTimezoneId(String timezoneId) {
+        this.timezoneId = timezoneId;
+    }
+
+    @JsonProperty("localtime")
+    public String getLocaltime() {
+        return localtime;
+    }
+
+    @JsonProperty("localtime")
+    public void setLocaltime(String localtime) {
+        this.localtime = localtime;
+    }
+
+    @JsonProperty("localtime_epoch")
+    public Integer getLocaltimeEpoch() {
+        return localtimeEpoch;
+    }
+
+    @JsonProperty("localtime_epoch")
+    public void setLocaltimeEpoch(Integer localtimeEpoch) {
+        this.localtimeEpoch = localtimeEpoch;
+    }
+
+    @JsonProperty("utc_offset")
+    public String getUtcOffset() {
+        return utcOffset;
+    }
+
+    @JsonProperty("utc_offset")
+    public void setUtcOffset(String utcOffset) {
+        this.utcOffset = utcOffset;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
